62ac9288824d197b8c8c15dff78c6cd8ef4fa318
Paruj Labpattanapaisan <mammothz@Parujs-MacBook-Pro.local>
2023-06-03 17:29:44 +0700

    feat:index 0,1

--- controllers/fibonacciController.js
+++ controllers/fibonacciController.js
@@ -2,26 +2,34 @@ const createError = require("../utils/createError");
 
 exports.nextFibonacci = async (req, res, next) => {
   try {
-    const { index } = req.query;
-    
+    const { index, click } = req.query;
+
     let result;
-    function fibonacci(num) {
-      if (num < 2) {
-        return num;
+    function fibonacci(index, click) {
+      if (index === 0) {
+        if (click === "increase") {
+          return index + 1;
+        } else {
+          return index - 1;
+        }
+      } else if (index === 1) {
+        if (click === "increase") {
+          return index;
+        } else {
+          return index - 1;
+        }
       } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+        return fibonacci(index - 1, click) + fibonacci(index - 2, click);
       }
     }
 
-    if (index <= 0) {
+    if (index < 0) {
       createError("Cannot be reduced by more than 0", 400);
     } else {
-      for (let i = 0; i < index; i++) {
-        result = fibonacci(i);
-        
+      for (let i = 0; i <= index; i++) {
+        result = fibonacci(i, click);
       }
     }
-    
 
     res.json({ message: "get next fibonacci successful", result });
   } catch (err) {
